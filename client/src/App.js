7de7d776daa9a3d34123d134e587ed3327dd7000
Charliemur2 <charliemurciamunoz@gmail.com>
2021-05-20 10:45:47 -0500

    feching data. first aproach

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import '../src/scss/styles.scss';
 import SearchBox from './components/SearchBox';
+import Results from './components/Results';
 
 const App = () => {
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    const fetchData = async () =>  {
+      const result = await axios(
+        'https://api.mercadolibre.com/items/MLA919739960'
+      );
+      setState(result.data);
+    };
+    fetchData();
+    console.log(state);
+  }, []);    
   return (
     <div className="App">
       <SearchBox />
+      <Results data={state}/>
+
     </div>
   );
 }
---
+++ client/src/components/Results.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../scss/styles.scss';
+
+const Results = (props) => {
+  console.log(props.data);
+  return (
+    <main className="main">
+
+    </main>
+  )
+}
+
+export default Results;
