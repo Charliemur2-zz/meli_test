4149130038748567a4212bdeda5736d443c461df
Charliemur2 <charliemurciamunoz@gmail.com>
2021-05-21 01:37:41 -0500

    fetchin API data to get data into Results componnent

--- client/src/components/Results.jsx
+++ client/src/components/Results.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import '../scss/styles.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
-const Results = (props) => {
-  console.log(props.data);
+const Results = ({term}) => {
+  console.log(term);
+  let uri = 'http://localhost:8081/api/items';
+  if (term) {
+    uri += `?q=${term}`;
+  }
+  console.log(uri);
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    const fetchData = async () =>  {
+      const result = await axios(uri);
+      setState(result.data);
+    };
+    fetchData();
+    console.log(state);
+  }, [uri, state]);    
   return (
     <main className="main">
 
--- client/src/components/SearchBox.jsx
+++ client/src/components/SearchBox.jsx
@@ -3,7 +3,13 @@ import '../scss/styles.scss';
 import logo from '../images/logo_ml.png';
 import search from '../images/search.png';
 
-const SearchBox = () => {
+const SearchBox = ({setTerm}) => {
+  const getItem = (event) => {
+    setTerm(event.target.value);
+  }
+  const onSubmitForm = (event) => {
+    event.preventDefault();
+  }
   return (
     <div className="search-box" role="search">
       <div className="wrapper">
@@ -13,8 +19,8 @@ const SearchBox = () => {
               <img className="seach__img" src={logo} alt="logo" />
             </a>
           </div>
-          <form className="search">
-            <input className="search__input" type="text" placeholder="search" name="term" role="term" />
+          <form className="search" onSubmit={event => onSubmitForm(event)}>
+            <input className="search__input" type="text" placeholder="search" name="term" role="term" onChange={event => getItem(event)}/>
             <button className="search__btn">
               <img className="search__btn-icon" src={search} alt="search-icon" />
             </button>
